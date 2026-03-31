0da357ac0abb32b25b4d61f3fa17bc3203e8bb80
Nathanaël Semhoun <nathanael@semhoun.net>
2026-03-31 12:03:05 +0200

    Replace php by frankenphp

--- docker/rootfs/opt/bin/entrypoint.sh
+++ docker/rootfs/opt/bin/entrypoint.sh
@@ -2,18 +2,11 @@
 
 set -e
 
-if [ ! -f "/etc/apache2/conf-docker/20-htdocs.conf" ]; then
-	cat > /etc/apache2/conf-docker/15-location.conf << EOF
-ServerName ${SERVER_NAME}
-ServerAdmin ${SERVER_ADMIN}
-EOF
-
-	if [ "${DEBUG_MODE}" == "true" ]; then
-		cat >  /etc/php/8.4/fpm/conf.d/99-debug.ini << 'EOF'
+if [ "${DEBUG_MODE}" == "true" ]; then
+	cat > /usr/local/etc/php/conf.d/99-debug.ini << 'EOF'
 display_errors = On
 display_startup_errors = On
 EOF
-	fi
 fi
 
 # Check and create $VAR_PATH and subfolders
@@ -29,10 +22,24 @@ if [ ! -d "/www/var/cache/proxy" ]; then
   su www-data -c "./console app:generate-proxies"
 fi
 
-rm -rf /var/spool/fcron/root
-/usr/bin/fcrontab -n /etc/fcron/fcrontab-root root
+mkdir -p /etc/caddy
+cat > /etc/caddy/Caddyfile << EOF
+{
+	auto_https off
+	frankenphp
+}
 
-rm -f /var/run/apache2.pid 
-rm -f /var/run/php-fpm.sock
+:80 {
+	root * /www/public
+	encode zstd gzip
+	php_server {
+		index index.php
+	}
+	file_server
+
+	@health path /health
+	header @health Cache-Control "no-store"
+}
+EOF
 
 exec "$@"
