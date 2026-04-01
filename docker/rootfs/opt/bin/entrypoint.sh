f9aae89c695db25948038056d5e7c8053901ba71
Nathanaël Semhoun <nathanael@semhoun.net>
2026-04-01 17:18:58 +0200

    Add telemetry to Frankenphp

--- docker/rootfs/opt/bin/entrypoint.sh
+++ docker/rootfs/opt/bin/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "${DEBUG_MODE}" == "true" ]; then
-	cat > /usr/local/etc/php/conf.d/99-debug.ini << 'EOF'
+  cat > /usr/local/etc/php/conf.d/99-debug.ini << 'EOF'
 display_errors = On
 display_startup_errors = On
 EOF
@@ -11,10 +11,10 @@ fi
 
 # Check and create $VAR_PATH and subfolders
 for dir in "${DATA_PATH}" "${DATA_PATH}/filer" "/www/var/cache" "/www/var/tmp"; do
-	if [ ! -d "$dir" ]; then
-		mkdir -p "$dir"
-	fi
-	chown www-data:www-data "$dir"
+  if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+  fi
+  chown www-data:www-data "$dir"
 done
 
 if [ ! -d "/www/var/cache/proxy" ]; then
@@ -23,46 +23,57 @@ if [ ! -d "/www/var/cache/proxy" ]; then
 fi
 
 mkdir -p /etc/caddy
-
-CADDY_GLOBAL_OPTIONS='	frankenphp'
-
 ACCESS_LOG_BLOCK=''
 if [ "${ENABLE_ACCESS_LOGS}" = "true" ]; then
-	ACCESS_LOG_BLOCK='	log {
-		output stdout
-		format console
-	}'
-	CADDY_GLOBAL_OPTIONS="${CADDY_GLOBAL_OPTIONS}
-${ACCESS_LOG_BLOCK}"
+  ACCESS_LOG_BLOCK='  log {
+    output stdout
+    format console
+  }'
 fi
-
+TRACING_BLOCK=''
+if [ -n "${OTEL_EXPORTER_OTLP_ENDPOINT}" ]; then
+TRACING_BLOCK='  tracing {
+      span "{method} {uri}"
+    }
+    request_header X-Trace-Id {http.vars.trace_id}
+'
+fi
 SITE_ADDRESS=':80'
+CADDY_GLOBAL_OPTIONS=''
 if [ "${ENABLE_LETSENCRYPT}" = "true" ]; then
-	SITE_ADDRESS="${SERVER_NAME}"
-	if [ -n "${ACME_EMAIL}" ]; then
-		CADDY_GLOBAL_OPTIONS="	email ${ACME_EMAIL}
+  SITE_ADDRESS="${SERVER_NAME}"
+  if [ -n "${ACME_EMAIL}" ]; then
+    CADDY_GLOBAL_OPTIONS="  email ${ACME_EMAIL}
 ${CADDY_GLOBAL_OPTIONS}"
-	fi
+  fi
 else
-	CADDY_GLOBAL_OPTIONS="	auto_https off
+  CADDY_GLOBAL_OPTIONS="  auto_https off
 ${CADDY_GLOBAL_OPTIONS}"
 fi
 cat > /etc/caddy/Caddyfile << EOF
 {
-${CADDY_GLOBAL_OPTIONS}
+  frankenphp
+  ${CADDY_GLOBAL_OPTIONS}
 }
 
 ${SITE_ADDRESS} {
-	root * /www/public
-	encode zstd gzip
-${ACCESS_LOG_BLOCK}
-	php_server {
-		index index.php
-	}
-	file_server
+  root * /www/public
+  encode zstd gzip
 
-	@health path /health
-	header @health Cache-Control "no-store"
+  php_server {
+    index index.php
+  }
+  file_server
+
+${ACCESS_LOG_BLOCK}
+  handle /health {
+  }
+  handle /* {
+${TRACING_BLOCK}
+  }
+
+  @health path /health
+  header @health Cache-Control "no-store"
 }
 EOF
 
