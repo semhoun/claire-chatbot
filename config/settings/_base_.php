661bc901a73feee01659442a5e913754275512e0
Nathanaël Semhoun <nathanael@semhoun.net>
2026-04-08 00:41:23 +0200

    Upgrade version to 1.4.0

--- config/settings/_base_.php
+++ config/settings/_base_.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Services\Settings;
 
 return [
-    'version' => '1.3.0',
+    'version' => '1.4.0',
     'debug' => env('DEBUG_MODE', false),
     'temporary_path' => Settings::getAppRoot() . '/var/tmp',
     'cache_dir' => Settings::getAppRoot() . '/var/cache',
