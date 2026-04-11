50f4946fd3637ade25fe6eacfb60bd8a424ca86a
Nathanaël Semhoun <nathanael@semhoun.net>
2026-04-11 18:25:57 +0200

    Fix for mysql

--- config/settings/database.php
+++ config/settings/database.php
@@ -24,7 +24,7 @@ return [
             'table_storage' => [
                 'table_name' => 'db_version',
                 'version_column_name' => 'version',
-                'version_column_length' => 1024,
+                'version_column_length' => 64,
                 'executed_at_column_name' => 'executed_at',
                 'execution_time_column_name' => 'execution_time',
             ],
