1108f32da21548df3ca6a60e75e918e71687d911
Nathanaël Semhoun <nathanael@semhoun.net>
2026-04-29 21:37:54 +0200

    Remove Mermaid

--- src/Services/Markdown.php
+++ src/Services/Markdown.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Renderer\ExtendedFencedCodeRendered;
 use Iamgerwin\PdfToMarkdownParser\PdfToMarkdownParser;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
-use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
 use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
 use League\CommonMark\Extension\Highlight\HighlightExtension;
 use League\CommonMark\Extension\SmartPunct\SmartPunctExtension;
@@ -17,7 +15,6 @@ use League\CommonMark\Extension\Table\TableExtension;
 use League\CommonMark\MarkdownConverter;
 use League\HTMLToMarkdown\Converter\TableConverter;
 use League\HTMLToMarkdown\HtmlConverter;
-use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
 use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
 use Twig\Extra\Markdown\MarkdownInterface;
 
@@ -34,7 +31,6 @@ readonly class Markdown implements MarkdownInterface
         $environment->addExtension(new HighlightExtension());
         $environment->addExtension(new TableExtension());
         $environment->addExtension(new CommonMarkCoreExtension());
-        $environment->addRenderer(FencedCode::class, new ExtendedFencedCodeRendered(new FencedCodeRenderer()));
         $environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer());
 
         $this->markdownConverter = new MarkdownConverter($environment);
